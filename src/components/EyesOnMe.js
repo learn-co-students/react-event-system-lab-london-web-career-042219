8095218575836b06b35c030f31f91edd072e04bf
MarcoMiranda05 <marmirand@hotmail.com>
2019-06-25 10:07:54 +0100

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React, { Component } from "react";
+
+class EyesOnMe extends Component {
+  handleFocus = e => {
+    console.log("Good!");
+  };
+
+  handleBlur = e => {
+    console.log("Hey! Eyes on me!");
+  };
+
+  render() {
+    return (
+      <button onBlur={this.handleBlur} onFocus={this.handleFocus}>
+        Submit
+      </button>
+    );
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
-// Code Keypad Component Here
+import React, { Component } from "react";
+
+class Keypad extends Component {
+  handlerKeyUp = e => {
+    console.log("Entering password...");
+  };
+
+  render() {
+    return <input type="password" onKeyUp={this.handlerKeyUp} />;
+  }
+}
+
+export default Keypad;
