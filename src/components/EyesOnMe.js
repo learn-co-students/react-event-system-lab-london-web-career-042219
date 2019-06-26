0d03058a7d955171f1283f5ec5ea6fe8087df2ab
Ross Mawdsley <rossmawd@gmail.com>
2019-06-26 12:25:24 +0100

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,30 @@
 // Code EyesOnMe Component Here
+
+import React, { Component } from "react";
+
+export default class EyesOnMe extends Component {
+  constructor(props) {
+    super(props); // Remember to call super() on the first line of the constructor
+    //(this is required in React components if we are to use this in the constructor)
+
+    this.state = {
+      secondsLeft: props.initialCount
+    };
+  }
+
+  onFocusFunction = () => {
+    console.log("Good!");
+  };
+  onBlurFunction = () => {
+    console.log("Hey! Eyes on me!");
+  };
+
+  render() {
+    //return <div id="matrix">{this.genMatrix()}</div>;
+    return (
+      <button onFocus={this.onFocusFunction} onBlur={this.onBlurFunction}>
+        I'm an idiot
+      </button>
+    );
+  }
+}
